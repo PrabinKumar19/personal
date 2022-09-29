6a3b606bd2bfbff9e5ffb4ab0ebac9435beb1e7f
Prabin Kumar <prabinkumar1999@outlook.com>
2022-09-29 18:04:25 +0400

    page updated

--- src/components/EachTask/index.jsx
+++ src/components/EachTask/index.jsx
@@ -1,15 +1,46 @@
+import { Checkbox } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
 
 function EachTask({ color, task }) {
   return (
     <TaskBody>
-      <div></div>
-      <p></p>
+      <div>
+        <div
+          style={{
+            backgroundColor: `${color}`,
+          }}
+        ></div>
+        <p>{task}</p>
+      </div>
+      <Checkbox />
     </TaskBody>
   );
 }
 
 export default EachTask;
 
-const TaskBody = styled.div``;
+const TaskBody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px 0;
+  gap: 20px;
+
+  > div {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  > div > div {
+    width: 5px;
+    height: 20px;
+    background-color: red;
+  }
+
+  > div > p {
+    color: #6b6b6b;
+    font-weight: 500;
+  }
+`;
--- src/pages/ToDoList/index.jsx
+++ src/pages/ToDoList/index.jsx
@@ -1,25 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase";
+import { useForm, Controller } from "react-hook-form";
 import { Button, IconButton } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import EachTask from "../../components/EachTask";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 function ToDoList() {
+  const [open, setOpen] = useState(false);
   const [user] = useAuthState(auth);
-  console.log(user.providerData[0].uid);
+  const {
+    handleSubmit,
+    setValue,
+    control,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      task_name: "",
+    },
+  });
 
-  // taskname, color
+  const onSubmit = (values) => console.log(values);
+  // console.log(user.providerData[0].uid);
+
+  const handleClose = () => {
+    setValue("task_name", "This is being tested");
+    // setOpen(false);
+  };
 
   return (
     <ToDoListMain>
+      <Dialog open={open} onClose={handleClose}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <DialogTitle>Add Task</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please add your task from the following form.
+            </DialogContentText>
+            <Controller
+              name={"task_name"}
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <TextField
+                  onChange={onChange}
+                  value={value}
+                  label="Task Name"
+                  fullWidth
+                  autoFocus
+                  margin="dense"
+                  variant="standard"
+                />
+              )}
+              rules={{ required: true }}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button type="submit">Subscribe</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
       <TopToDoList>
         <p>Happy to add your task</p>
-        <Button color="primary" aria-label="Add a task" variant="contained">
-          <AddIcon />
-        </Button>
+        <IconButton
+          color="primary"
+          aria-label="Add a task"
+          variant="contained"
+          onClick={() => setOpen(true)}
+        >
+          <AddCircleIcon fontSize="large" />
+        </IconButton>
       </TopToDoList>
-      <BodyToDoList></BodyToDoList>
+      <BodyToDoList>
+        <div>
+          <p>Title</p>
+          <p>Completed</p>
+        </div>
+        <div>
+          <EachTask
+            color={"Red"}
+            task={
+              "Guys when you add music, Just see if you have a list, start_radio at the end which means it’s a playlist rather than a single song. Just be cautious on that one."
+            }
+          />
+          <EachTask color={"Yellow"} task={"Need to complete this task"} />
+          <EachTask color={"Green"} task={"Need to complete this task"} />
+        </div>
+      </BodyToDoList>
     </ToDoListMain>
   );
 }
@@ -27,12 +101,32 @@ function ToDoList() {
 export default ToDoList;
 
 const ToDoListMain = styled.div`
+  margin: 1rem;
   padding: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  border-radius: 10px;
 `;
 
 const TopToDoList = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  border-bottom: 1px solid lightgray;
+  padding: 10px 0;
+  margin-bottom: 10px;
+
+  > p {
+    font-weight: 700;
+  }
 `;
-const BodyToDoList = styled.div``;
+const BodyToDoList = styled.div`
+  padding: 10px 0;
+
+  > div:nth-child(1) {
+    display: flex;
+    justify-content: space-between;
+    color: gray;
+    font-size: 14px;
+    font-weight: 700;
+  }
+`;
